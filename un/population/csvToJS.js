95b0044b8b0f284481087a9f379b655c87f5ab1a
Curran <curran.kelleher@gmail.com>
2013-08-14 11:35:19 -0400

    Added AMD version of UN Population Data

---
+++ un/population/csvToJS.js
@@ -0,0 +1,21 @@
+// Run this Node.js script to create an AMD module from a CSV file.
+var d3 = require('d3'),
+    _ = require('underscore'),
+    fs = require('fs'),
+    name = 'populationEstimates';
+
+fs.readFile('./'+name+'.csv', 'utf8', function (err, data) {
+  var data = d3.csv.parse(data),
+      script = [
+        'define([], function(){ return ',
+        JSON.stringify(data),
+        ';});'
+      ].join('');
+  fs.writeFile('./'+name+'.js', script, function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("Wrote '"+name+".js'!");
+    }
+  }); 
+})
