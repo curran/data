742bc0da20bc14a018496071b59d7ca026a82182
curran <curran.kelleher@gmail.com>
2014-02-13 15:14:07 -0500

    Added cleaned and simplified mortality data

--- cdc/mortality/csvToJS.js
+++ cdc/mortality/csvToJS.js
@@ -3,16 +3,18 @@
 //
 // By Curran Kelleher 2/13/2014
 //
-// first run 'npm install -g d3'
+// first run 'npm install d3 underscore'
 // then run this script with 'node csvToJS.js'
 
 var d3 = require('d3'),
+    _ = require('underscore'),
     fs = require('fs'),
-    inputFile = './mortality_underyling_cause_by_years.csv',
+    inputFile = './mortality_underyling_cause_by_years_simplified.csv',
     outputFile = './mortality.js';
 
 fs.readFile(inputFile, 'utf8', function (err, data) {
-  var data = d3.csv.parse(data),
+  var rawData = d3.csv.parse(data),
+      data = pivot(rawData),
       script = [
         'define([], function(){ return ',
         JSON.stringify(data),
@@ -26,3 +28,33 @@ fs.readFile(inputFile, 'utf8', function (err, data) {
     }
   }); 
 })
+
+// A dataset-specific function that pivots the table
+// such that each entry in the resulting data set is a
+// single year.
+function pivot(rawData) {
+  var minYear = 1981,
+      maxYear = 2010,
+      years = _.range(minYear, maxYear + 1),
+      entriesByCause = {},
+      causes,
+      data;
+
+  // Index entries by cause.
+  rawData.forEach(function (entry) {
+    entriesByCause[entry.Cause] = entry;
+  });
+
+  // Compute the list of causes from the index.
+  causes = _.keys(entriesByCause);
+
+  // Combine causes for each year.
+  data = _.map(years, function (year) {
+    var yearEntry = {year: year};
+    causes.forEach(function (cause) {
+      yearEntry[cause] = entriesByCause[cause][year];
+    });
+    return yearEntry;
+  });
+  return data;
+}
