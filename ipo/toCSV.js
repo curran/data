d30222df2cc0b21f681203ced8fdec34c502e05d
curran <curran.kelleher@gmail.com>
2015-08-05 18:03:28 -0700

    Quote strings in CSV

--- ipo/toCSV.js
+++ ipo/toCSV.js
@@ -2,7 +2,12 @@ module.exports = function toCSV(data){
   var columns = Object.keys(data[0]);
   return [columns.join(",")].concat(data.map(function(d){
     return columns.map(function(column){
-      return d[column];
+      var value = d[column];
+      if(typeof value === "string"){
+        return '"' + value + '"';
+      } else {
+        return value
+      }
     }).join(",");
   })).join("\n");
 }
